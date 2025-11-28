1813dc570cb793f6e51b61848d2bf99172ab2610
kadriandev <kyle.monteiro@gmail.com>
2025-11-27 20:43:48 -0500

    added sentry

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,6 @@
-export default {
+import { withSentryConfig } from "@sentry/nextjs";
+
+const nextConfig = {
   /* config options here */
   output: "standalone",
   eslint: {
@@ -7,3 +9,17 @@ export default {
     ignoreDuringBuilds: true,
   },
 };
+
+// Make sure adding Sentry options is the last code to run before exporting
+export default withSentryConfig(nextConfig, {
+  org: "kadriandev",
+  project: "fantasy-tools",
+  // Only print logs for uploading source maps in CI
+  // Set to `true` to suppress logs
+  silent: !process.env.CI,
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+  reactComponentAnnotation: {
+    enabled: true,
+  },
+});

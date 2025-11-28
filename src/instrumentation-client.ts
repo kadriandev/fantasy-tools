e4bb854842c3b496c2569200714057a08498c3e3
kadriandev <kyle.monteiro@gmail.com>
2025-11-27 20:43:48 -0500

    added sentry

---
+++ src/instrumentation-client.ts
@@ -0,0 +1,13 @@
+import * as Sentry from "@sentry/nextjs";
+Sentry.init({
+  dsn: "https://ec717fcb7a33bf7bcecb0a0e90209b7a@o4510319943745536.ingest.us.sentry.io/4510319944728576",
+  // Adds request headers and IP for users, for more info visit:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
+});
+// This export will instrument router navigations, and is only relevant if you enable tracing.
+// `captureRouterTransitionStart` is available from SDK version 9.12.0 onwards
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;

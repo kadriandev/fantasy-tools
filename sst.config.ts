14332049714096a0e3712e6a1873b31b6e384651
kadriandev <kyle.monteiro@gmail.com>
2026-03-17 13:33:41 -0400

    set removal policy for prod to remove to decommission

--- sst.config.ts
+++ sst.config.ts
@@ -1,34 +1,34 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 export default $config({
-  app(input) {
-    return {
-      name: "fantasy-tools",
-      removal: input?.stage === "prod" ? "retain" : "remove",
-      protect: ["prod"].includes(input?.stage),
-      home: "aws",
-      providers: { cloudflare: "6.10.0" },
-    };
-  },
-  async run() {
-    const { readdirSync } = await import("fs");
-    const outputs = {};
-    for (const value of readdirSync("./infra/")) {
-      const result = await import("./infra/" + value);
-      if (result.outputs) Object.assign(outputs, result.outputs);
-    }
-    return outputs;
-  },
-  console: {
-    autodeploy: {
-      target(event) {
-        if (
-          event.type === "branch" &&
-          event.branch === "master" &&
-          event.action === "pushed"
-        ) {
-          return { stage: "prod" };
-        }
-      },
-    },
-  },
+	app(input) {
+		return {
+			name: "fantasy-tools",
+			removal: input?.stage === "prod" ? "remove" : "remove",
+			protect: ["prod"].includes(input?.stage),
+			home: "aws",
+			providers: { cloudflare: "6.10.0" },
+		};
+	},
+	async run() {
+		const { readdirSync } = await import("fs");
+		const outputs = {};
+		for (const value of readdirSync("./infra/")) {
+			const result = await import("./infra/" + value);
+			if (result.outputs) Object.assign(outputs, result.outputs);
+		}
+		return outputs;
+	},
+	console: {
+		autodeploy: {
+			target(event) {
+				if (
+					event.type === "branch" &&
+					event.branch === "master" &&
+					event.action === "pushed"
+				) {
+					return { stage: "prod" };
+				}
+			},
+		},
+	},
 });
